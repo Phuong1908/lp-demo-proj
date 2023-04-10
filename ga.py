4591929ca6d4c9fc8ffbbf89e0e2777fb5e7c633
phuongn <thuphuong.nguyen.cntt@gmail.com>
2023-04-10 20:00:50 +0700

    implement fitness function

--- ga.py
+++ ga.py
@@ -1,18 +1,30 @@
 import numpy as np
 import random
+from utils import intersection
+from constants import ALPHA
 class Chromosome:
   def __init__(self, length):
     self.length = length
     self.bit_arr = np.array([random.randint(0, 1) for _ in length])
   
-  def calc_cost(self):
-    return
+  def calc_cost(self, cost_list):
+    return np.dot(self.bit_arr, cost_list)
   
-  def calc_error(self):
-    return
+  def get_uncovered_count(self, row_covered_list):
+    count = 0
+    self_cover_rows = np.where(self.bit_arr == 1)
+    for row_covered_data in row_covered_list:
+      if intersection(self_cover_rows, row_covered_data):
+        count += 1
+    return count
   
-  def calc_fitness():
-    return
+  def calc_error(self, cost_list):
+    max_cost = np.max(np.matmul(self.bit_arr, cost_list))
+    uncovered_count = self.get_uncovered_count()
+    return ALPHA*uncovered_count*max_cost
+    
+  def calc_fitness(self):
+    return -(self.calc_error() + self.calc_cost())
   
   @classmethod
   def crossover(cls):
