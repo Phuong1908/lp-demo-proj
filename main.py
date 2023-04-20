53b54208eac59e56a55364d8a30735572edd39ff
phuongn <thuphuong.nguyen.cntt@gmail.com>
2023-04-20 10:16:21 +0700

    Fix bug on main script & add print lines to keep track status of generations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import argparse
 import copy
+import numpy as np
 from ga import Population
 from utils import build_problem
 from constants import GENERATIONS, POPULATION_SIZE
@@ -11,21 +12,18 @@ if __name__ == "__main__":
   parser.add_argument("problem_name", help="Name of scp problem")
   args = parser.parse_args()
   problem_name = args.problem_name
-  
   problem = build_problem(name=problem_name)
+  print(f'Max cost: {np.sum(problem.costs)}')
   population = Population.create_initial_population(problem=problem)
   best_invi = None
   for i in tqdm(range(GENERATIONS)):
 		# evolution phase
     children = population.create_child()
-    # import pdb;pdb.set_trace()
     population.extend(children)
-    new_population = copy.copy(population)
-    local_best_indi = population.get_best()
 		# evaluate phase
-    new_population.extend(sorted(population.population, key=lambda x: x.fitness, reverse=True)[0:POPULATION_SIZE])
-    best_invi = new_population.population[0]
-    population = new_population
-  print(f'Best solution found has cost {best_invi.calc_cost(cost_list=problem.costs)}')
+    new_population = (sorted(population.population, key=lambda x: x.fitness, reverse=True)[0:POPULATION_SIZE])
+    best_invi = new_population[0]
+    population.population = new_population
+    print(f'Best individual of gen {i} has cost: {best_invi.calc_cost(cost_list=problem.costs)}')
   print(f'Optimal cost is {problem.optimal_sol}')
     
